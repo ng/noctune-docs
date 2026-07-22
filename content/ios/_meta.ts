fa8a1559697baa8625d42d725b32565770caf6f6
Jonathan Ng <jon@jonathanng.com>
2026-07-21 23:30:30 -0700

    fix: hide iOS legal pages from docs navigation

---
+++ content/ios/_meta.ts
@@ -0,0 +1,7 @@
+export default {
+  support: 'Noctune for iOS Support',
+  privacy: { display: 'hidden' },
+  terms: { display: 'hidden' },
+  'ca-privacy': { display: 'hidden' },
+  'do-not-sell': { display: 'hidden' },
+}
